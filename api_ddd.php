92fc100258f6d6f79e0890b430d0c41233cb0f97
paulo henrique marques <paulohenriquemenezes00@gmail.com>
2024-03-03 00:31:56 -0300

    Ajuste func redundante

--- api_ddd.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(isset($_GET['ddd'])) {
-        $ddd = $_GET['ddd'];
-        $url = "https://brasilapi.com.br/api/ddd/v1/$ddd";
-        $response = file_get_contents($url);
-        echo $response;
-    }
-?>
--- index.php
+++ index.php
@@ -2,36 +2,26 @@
 <html>
 <head>
     <title>Consulta API</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <h1>Consulta API do Brasil app</h1>
     <div>
         <h2>consulta de qual regial é o ddd</h2>
-        <form action="api_ddd.php" method="GET">
+        <form method="GET">
             <label for="ddd">Digite o DDD:</label>
             <input type="text" name="ddd" id="cx_ddd" required>
             <button type="submit">Consultar</button>
-            <div id= "resp_ddd"></div>
+            <?php
+                if(isset($_GET['ddd'])) {
+                    $ddd = $_GET['ddd'];
+                    // chama a api
+                    $url = "https://brasilapi.com.br/api/ddd/v1/$ddd";
+                    // pega a resposta e coloca em um json
+                    $response = json_decode(file_get_contents($url), true);
+                    echo "<h2>Resultado:</h2><p>Região:".$response['state']."</p>";
+                }
+            ?>
         </form>
     </div>
-    <script>
-        $(document).ready(function() {
-            $('form').submit(function(event) {
-                event.preventDefault();
-                var ddd = $('#cx_ddd').val();
-                $.ajax({
-                    url: 'api_ddd.php',
-                    method: 'GET',
-                    data: { ddd: ddd },
-                    dataType: 'json',
-                    success: function(resp) {
-                        console.log(resp);
-                        $('#resp_ddd').html('<h2>Resultado:</h2><p>Região: ' + resp.state + '</p>');
-                    }
-                });
-            });
-        });
-    </script>
 </body>
 </html>
